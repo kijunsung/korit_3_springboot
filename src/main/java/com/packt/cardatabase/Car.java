92ed1cd34dc2de4ecc21641ea1fe2a1fa947b9a7
kijunsung <keejunsd@naver.com>
2025-05-12 15:00:56 +0900

    sddsd

---
+++ src/main/java/com/packt/cardatabase/Car.java
@@ -0,0 +1,88 @@
+package com.packt.cardatabase.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String brand, model, color, registrationNumber;
+
+    private int modelYear, price;
+
+    // 기본 생성자.
+    public Car() {
+    }
+
+    // id 필드를 제외한 나머지 필드에 대한 RequiredargsConstructor.
+    public Car(String brand, String model, String color, String registrationNumber, int modelYear, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+        this.modelYear = modelYear;
+        this.price = price;
+    }
+
+    // Getter / Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
